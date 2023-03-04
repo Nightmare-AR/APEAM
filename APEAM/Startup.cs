fe01310c0b3acb1ebbd64998e5ddd63805c2437b
Arturo AR <roman.xa7x@gmail.com>
2023-03-03 21:42:52 -0600

    Add project files.

---
+++ APEAM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APEAM.Startup))]
+namespace APEAM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
